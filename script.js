cf794b550864df1c4ccc69abb5c3a3130b55989c
santytorrico <santy.torrico@gmail.com>
2026-02-20 17:01:29 -0400

    refactor: redefine constructor and create method

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 class gameModel{
     constructor(){
-        this.board = [];
+        this.board = this.createBoard();
     }
 
     createBoard(){
         const board = [];
         for(let i = 0; i < 12 ; i++){
-            this.board.push({
+            board.push({
                 id: i,
                 status: false
-
             });
         }
         return board;
@@ -24,4 +23,4 @@ class gameController{
 
 const game = new gameModel();
 console.log(game.createBoard());
-console.log(game.board[12].id);
+console.log(game.board[1].status);
